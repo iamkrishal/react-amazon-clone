386f914b23012640f89a0aaf192d63981589cdf4
iamkrishal <krishaladhakari2065@gmail.com>
2023-01-07 10:12:16 +0545

    class name changed to className

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,14 +3,14 @@ import "./product.css";
 function Product(props) {
   return (
     <div className="product_container">
-      <div class="product-card">
-        <img src={props.image} alt="Product Name" class="product-image" />
+      <div className="product-card">
+        <img src={props.image} alt="Product Name" className="product-image" />
 
         <div className="product__content">
-          <h3 class="product-name">{props.title}</h3>
-          <p class="product-description">{props.description}</p>
-          <div class="product-price">{props.price}</div>
-          <button class="add-to-cart-button">Add to Cart</button>
+          <h3 className="product-name">{props.title}</h3>
+          <p className="product-description">{props.description}</p>
+          <div className="product-price">{props.price}</div>
+          <button className="add-to-cart-button">Add to Cart</button>
         </div>
       </div>
     </div>
